7dbd9dfb3da4b4e78d32fbe2ec1c95a0a2f3c900
Bruno Bagattini Fernandes <brbagattinif@gmail.com>
2025-11-21 21:21:40 -0300

    Fix: correções para vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
-import Home from "./Routes/Home.jsx";
+import Home from "./routes/Home.jsx";
 import Login from "./routes/Login.jsx";
 import SobreNos from "./routes/SobreNos.jsx";
 import MeuPerfil from "./routes/MeuPerfil.jsx";
